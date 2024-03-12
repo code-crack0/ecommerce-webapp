d602ce077c45e786b192c7841f2ca85840755563
TaufiqSyed <tfqsyed@gmail.com>
2024-03-12 11:16:49 +0400

    Added truncation

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-
- import {BrowserRouter, Route, Routes} from 'react-router-dom';
- import SignInPage from './components/SignIn';
-  import SignUpPage from './components/SignUp';
-import HomePage from "./pages/HomePage";
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import SignInPage from './components/SignIn'
+import SignUpPage from './components/SignUp'
+import HomePage from './pages/HomePage'
 
 export default function App() {
   return (
-   <BrowserRouter>
+    <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage/>} />
-        <Route path="/sign-up" element={<SignUpPage/>} />
-        <Route path="/sign-in" element={<SignInPage/>} />
+        <Route path='/' element={<HomePage />} />
+        <Route path='/sign-up' element={<SignUpPage />} />
+        <Route path='/sign-in' element={<SignInPage />} />
       </Routes>
-   </BrowserRouter>
+    </BrowserRouter>
   )
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,54 +1,61 @@
 import React, { useEffect, useState } from 'react'
-import { useUser,UserButton} from '@clerk/clerk-react'
-import {Link} from 'react-router-dom'
+import { useUser, UserButton } from '@clerk/clerk-react'
+import { Link } from 'react-router-dom'
 import axios from 'axios'
 const HomePage = () => {
-    const {isLoaded, user} = useUser();
-    const [data,setData] = useState([]);
-    const fetchData = async () => {
-        const response =  await axios.get("https://fakestoreapi.com/products")
-        setData(response.data)
-    }
-    useEffect(() => {
-        fetchData()
-    }
-    ,[])
+  const { isLoaded, user } = useUser()
+  const [data, setData] = useState([])
+  const fetchData = async () => {
+    const response = await axios.get('https://fakestoreapi.com/products')
+    setData(response.data)
+  }
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  function truncString(s, charCount) {
+    if (s.length < charCount) return s
+    const tmp = s.substring(0, charCount)
+    const res = tmp.substring(0, tmp.lastIndexOf(' ')) + '...'
+    return res
+  }
   return (
     <>
-    {
-            isLoaded && user ? (
-            <div>
-            <h1>Welcome USER!</h1>
-            <h1>Welcome {user.fullName +  ' ' + user.primaryEmailAddress}</h1>
-            <UserButton afterSignOutUrl='/sign-in'/>
-            </div>
-        )
-        :(
-            <div>
-                You are not Signed In.
-                <Link to='/sign-in'>
-                    Sign In
-                </Link>
-            </div>
-        )
+      {isLoaded && user ? (
+        <div>
+          <h1>Welcome USER!</h1>
+          <h1>Welcome {user.fullName + ' ' + user.primaryEmailAddress}</h1>
+          <UserButton afterSignOutUrl='/sign-in' />
+        </div>
+      ) : (
+        <div>
+          You are not Signed In.
+          <Link to='/sign-in'>Sign In</Link>
+        </div>
+      )}
+      <div id='wrapper'>
+        <div className='items'>
+          {data &&
+            data.map((item) => {
+              return (
+                <div key={item.id} className='single_item'>
+                  <div className='single_item_title'>
+                    <h1>{truncString(item.title, 40)}</h1>
+                  </div>
 
-
-    }
-    <div className='items'>
-        {
-            data && data.map((item) => {
-                return (
-                    <div key={item.id} className='single_item'>
-                        <div className='single_item_title'><h1>{item.title}</h1></div>
-                        
-                        <img src={item.image} alt={item.title} height={100} width={100} />
-                        <p>{item.description.length > 80 ? item.description.substring(0, 80) + '...' : item.description}</p>
-                        <p>{item.price}</p>
-                    </div>
-                )
-            })
-        }
-    </div>
+                  <img
+                    src={item.image}
+                    alt={item.title}
+                    height={100}
+                    width={100}
+                  />
+                  <p>{truncString(item.description, 80)}</p>
+                  <p>{item.price}</p>
+                </div>
+              )
+            })}
+        </div>
+      </div>
     </>
   )
 }
