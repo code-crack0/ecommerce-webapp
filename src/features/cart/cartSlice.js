0db7b55a56ff6cd7fd4f406f4fa5ba3a87a2f49e
Samad Sayed <samaddxb004@gmail.com>
2024-03-16 10:42:11 +0400

    cart functionality

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,20 +1,25 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = []
+const initialState = JSON.parse(localStorage.getItem('cart')) || [];
 
 export const cartSlice = createSlice({
   name: 'cart',
   initialState,
   reducers: {
     addToCart: (state, action) => {
-      state.push(action.payload)
+      const existingCart = JSON.parse(localStorage.getItem('cart')) || [];
+      const updatedCart = [...existingCart, action.payload];
+      localStorage.setItem('cart', JSON.stringify(updatedCart));
+      return updatedCart;
     },
     removeFromCart: (state, action) => {
-      return state.filter((item) => item.id !== action.payload.id)
+      const updatedCart = state.filter((item) => item.id !== action.payload.id);
+      localStorage.setItem('cart', JSON.stringify(updatedCart));
+      return updatedCart;
     },
   },
-})
+});
 
-export const { addToCart, removeFromCart } = cartSlice.actions
+export const { addToCart, removeFromCart } = cartSlice.actions;
 
-export default cartSlice.reducer
+export default cartSlice.reducer;
